8befe7a2f4f4986794fef97034d5221d5147f637
Benjamin Lewis <benji@xfour.co.uk>
2019-08-05 01:33:17 +0100

    Slight Rebuild

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,57 @@
+const firebase = require('firebase-admin');
 const functions = require('firebase-functions');
+firebase.initializeApp();
+var db = firebase.firestore();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.createJSON = functions.region('europe-west2').https.onRequest((request, response) => {
+    response.set('Access-Control-Allow-Origin', '*');
+    var basicJSON1 = `{
+        "id": "places",
+        "type": "symbol",
+        "layout": {
+            "icon-image": "{icon}",
+            "icon-allow-overlap": true
+            },
+        "source": {
+            "type": "geojson",
+            "data": {
+                "type": "FeatureCollection",
+                "features": [`;
+                var basicJSON2 = `
+            ]}
+        }
+    }`;
+    var secondJSON = "";
+    var stringJSON = "";
+    db.collection("vanLocations").get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            console.log("DB: " + doc.data().name);
+            basicJSON1 = basicJSON1 + `{
+                "type": "Feature",
+                "properties": {
+                "description": "`+doc.data().description+`",
+                "icon": "custom-marker"
+                },
+                "geometry": {
+                "type": "Point",
+                "coordinates": [`+doc.data().location.longitude+`,`+doc.data().location.latitude+`]
+                }
+            },`})}).then(() => {
+                secondJSON = basicJSON1.slice(0, -1);
+                stringJSON = secondJSON+basicJSON2;
+                console.log(secondJSON);
+                console.log(stringJSON);
+            }).then(() => {
+                var responseJSON = JSON.parse(stringJSON);
+                response.send(200, responseJSON);
+            }).catch(error => {
+                    console.error("Error: " + error);
+                });
+});
+
+exports.getNames = functions.region('europe-west2').https.onRequest((request, response) => {
+    db.collection("vanLocations").get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            console.log("DB: " + doc.data().name);
+            console.log("Loc: " + doc.data().location)
+        })})});
--- public/lib/js/main.js
+++ public/lib/js/main.js
@@ -1,4 +1,4 @@
-const firebaseConfig = {
+/*const firebaseConfig = {
     apiKey: "AIzaSyBuXp-d0Dvvq3au_2yecyPm9ehdV4hCzFo",
     authDomain: "junehack.firebaseapp.com",
     databaseURL: "https://junehack.firebaseio.com",
@@ -7,10 +7,10 @@ const firebaseConfig = {
     messagingSenderId: "739569160546",
     appId: "1:739569160546:web:68baca3bf461fca2"
   };
-  firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp(firebaseConfig);*/
 
 var xmlHttp = new XMLHttpRequest();
-xmlHttp.open( "GET", "https://junehack.firebaseio.com/map.json", false ); // false for synchronous request
+xmlHttp.open( "GET", "https://europe-west2-kebab-ly.cloudfunctions.net/createJSON", false ); // false for synchronous request
 xmlHttp.send( null );
 var responseData = xmlHttp.responseText;
 
@@ -21,14 +21,15 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiYmVuamlzb2Z0IiwiYSI6ImNqeXJrcTFkODA0enEzb3N5e
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v11',
       center: [-0.9713584,51.4581074],
-      zoom: 11
+      zoom: 10
     });
 
 map.on('load', function() {
     map.loadImage("https://i.imgur.com/MK4NUzI.png", function(error, image) {
     if (error) throw error;
-    map.addImage("custom-marker", image)});
-    map.addLayer(kebabData);
+    map.addImage("custom-marker", image)
+    map.addLayer(kebabData)});
+    
 });
 
 map.on('click', 'places', function (e) {
