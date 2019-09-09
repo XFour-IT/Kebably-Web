33bda069a91fbabef7b3700dc8dff152c8f4129d
Benji Lewis <benji@benjisoft.org.uk>
2019-09-09 18:51:53 +0100

    Nearest point function

--- functions/index.js
+++ functions/index.js
@@ -49,9 +49,37 @@ exports.createJSON = functions.region('europe-west2').https.onRequest((request, 
                 });
 });
 
-exports.getNames = functions.region('europe-west2').https.onRequest((request, response) => {
-    db.collection("vanLocations").get().then((querySnapshot) => {
+exports.nearestPoint = functions.region('europe-west2').https.onRequest((request, response) => {
+    var closest
+    function rad(x) {return x*Math.PI/180;}
+    function findClosest(request) {
+        console.log("Running");
+        var location = request.body;
+        var lat = location.lat;
+        var lng = location.lng;
+        var i = 0
+        const R = 6371; // radius of earth in km
+        var distances = [];
+        closest = -1;
+        db.collection("vanLocations").get().then((querySnapshot) =>
         querySnapshot.forEach((doc) => {
-            console.log("DB: " + doc.data().name);
-            console.log("Loc: " + doc.data().location)
-        })})});
+            console.log("Testing: "+doc.data().name);
+            i += 1;
+            var mlat = doc.data().location.latitude;
+            var mlng = doc.data().location.longitude;
+            var dLat  = rad(mlat - lat);
+            var dLong = rad(mlng - lng);
+            var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+                Math.cos(rad(lat)) * Math.cos(rad(lat)) * Math.sin(dLong/2) * Math.sin(dLong/2);
+            var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+            var d = R * c;
+            distances[i] = d;
+            if ( closest == -1 || d < distances[closest] ) {
+                closest = doc.data().location;
+                console.log(closest);
+                response.send(200, closest)
+            }
+        }));
+    }
+    findClosest(request);
+})
